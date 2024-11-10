7496cc7ee145a3d3cfc326a4d6902e4fa4a42c44
Mauro Felipe <mfelipxl@gmail.com>
2024-11-10 13:49:54 -0400

    add a effect on icons at section services.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,13 @@
-import { Container, Database, FileCode2 } from 'lucide-react'
+import { Server, Database, FileCode2 } from 'lucide-react'
 
 const Services = () => {
+  const shadowOnIcons = {
+    maskImage: 'linear-gradient(to top left, rgba(0, 0, 0, 0) 10%, rgba(0, 0, 0, 1) 70%)',
+    WebkitMaskImage: 'linear-gradient(to top left, rgba(0, 0, 0, 0) 5%, rgba(0, 0, 0, 1) 60%)',
+  }
   return (
     <div className="w-full mt-32 p-12">
-      <div className="font-bold flex justify-center text-4xl mb-12">
+      <div className="font-bold flex justify-center text-4xl mb-16">
         <h1>
           Services
           <span className="bg-gradient-to-r from-[#FF0000] to-pink-900 bg-clip-text text-transparent">.</span>
@@ -13,7 +17,7 @@ const Services = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="bg-stone-900 p-6 rounded-lg border-2 border-white border-opacity-10 hover:border-red-600 transition-all duration-700">
           <div className='py-5'>
-            <FileCode2 size={80} className="text-red-600"/>
+            <FileCode2 size={80} className="text-red-600" style={shadowOnIcons}/>
           </div>
           <h2 className="text-2xl font-bold mb-2">Front-end</h2>
           <p className="text-gray-600">Create beautiful and responsive user interfaces with modern frameworks like React, Vue, and Angular. I specialize in building interactive front-end experiences that delight users and enhance engagement.</p>
@@ -21,7 +25,7 @@ const Services = () => {
 
         <div className="bg-stone-900 p-6 rounded-lg border-2 border-white border-opacity-10 hover:border-red-600 transition-all duration-700">
           <div className='py-5'>
-            <Database size={80} className="text-red-600"/>
+            <Database size={80} className="text-red-600" style={shadowOnIcons}/>
           </div>
           <h2 className="text-2xl font-bold mb-2">Back-end</h2>
           <p className="text-gray-600">Develop robust and scalable back-end systems using technologies like Node.js, Python, and databases such as PostgreSQL and MongoDB. I build the server-side logic that powers your application and handles complex data processing.</p>
@@ -29,7 +33,7 @@ const Services = () => {
 
         <div className="bg-stone-900 p-6 rounded-lg border-2 border-white border-opacity-10 hover:border-red-600 transition-all duration-700">
           <div className='py-5'>
-            <Container size={80} className="text-red-600"/>
+            <Server size={80} className="text-red-600" style={shadowOnIcons}/>
           </div>
           <h2 className="text-2xl font-bold mb-2">DevOps</h2>
           <p className="text-gray-600">Ensure your application runs smoothly and scales with ease through continuous integration and delivery (CI/CD), containerization with Docker, and infrastructure management with Kubernetes. I help automate your workflows and optimize deployments.</p>
